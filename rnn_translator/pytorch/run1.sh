fbf4111d827d534e639140761a8c8ad76288f6ac
EC2 Default User <ec2-user@ip-172-31-17-75.us-east-2.compute.internal>
2020-02-24 03:16:16 +0000

    multiple dispatch

--- rnn_translator/pytorch/run1.sh
+++ rnn_translator/pytorch/run1.sh
@@ -8,7 +8,7 @@ SEED=${1:-"1"}
 TARGET=${2:-"24.00"}
 
 # run training
-python3 train_parallel.py  \
+python3 train_parallel1.py  \
   --dataset-dir ${DATASET_DIR} \
   --seed $SEED \
   --target-bleu $TARGET
